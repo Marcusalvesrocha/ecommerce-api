ca01c2f67370f936cd185d050fd30cb18eaf4a2a
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-29 17:06:54 -0300

    view template

--- test/mailers/previews/devise/mailer_preview.rb
+++ test/mailers/previews/devise/mailer_preview.rb
@@ -1,5 +1,6 @@
 class Devise::MailerPreview < ActionMailer::Preview
   def reset_password_instructions
+    # view template
     Devise::Mailer.reset_password_instructions(User.first, {})
   end
 end
